7f4dea02819a022e6e8bcf7cfc042a734987db27
harvestcore <aagomezies@gmail.com>
2023-10-18 00:57:44 +0200

    #1 Fix video pause bug + add some logs

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 console.log("[YouTube Adblock popup remover v1.0.0]");
 
+function isVideoPlaying(video) {
+    return video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2;
+}
+
 // https://stackoverflow.com/a/54389066
 document.addEventListener("yt-page-data-updated", () => {
-    document.querySelector("ytd-popup-container")?.remove();
-    document.querySelector("video")?.play();
+    const popup = document.querySelector("ytd-popup-container");
+    if (popup) {
+        console.log("YT adblock popup found!");
+        popup.remove();
+    }
+
+    setTimeout(() => {
+        const video = document.querySelector("video");
+        if (video && !isVideoPlaying(video)) {
+            video.play();
+        }
+    }, 500);
 });
